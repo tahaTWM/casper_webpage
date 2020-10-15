22d07736a78fba8a1e9b40a0a431843b679462cd
tahaTWM <taha.twm.1234@gmail.com>
2020-10-15 23:20:53 +0300

    1

--- update repo.bat
+++ update repo.bat
@@ -4,7 +4,7 @@ set /p repoName=Enter your local repository Name :
 
 set /p repoLink=Enter you repository Name : 
 git init
-git remote add %repoName% https://github.com/tahaTWM/%repoName%
+git remote add %repoName% https://github.com/tahaTWM/%repoLink%
 git add -A
 set /p repoCommit=Enter your repo commant : 
 git commit -m "%repoCommit%"
