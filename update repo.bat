fe92d96e5e5494882dbbf1610bcb60edd1c9bf0f
tahaTWM <taha.twm.1234@gmail.com>
2020-10-15 23:17:56 +0300

    first commit

---
+++ update repo.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set /p repoName=Enter your local repository Name : 
+
+set /p repoLink=Enter you repository Name : 
+git init
+git remote add %repoName% https://github.com/tahaTWM/%repoName%
+git add -A
+set /p repoCommit=Enter your repo commant : 
+git commit -m "%repoCommit%"
+git push -u %repoName% master
+pause
