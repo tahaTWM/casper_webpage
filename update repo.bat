0c93964fa26b03fea97d52505efaa48f74368f1c
tahaTWM <taha.twm.1234@gmail.com>
2020-10-16 09:02:10 +0300

    first Commit

--- update repo.bat
+++ update repo.bat
@@ -1,14 +1,26 @@
 @echo off
 
-set /p loaclName=Enter your local repository Name : 
-
-set /p repoName=Enter you repository Name : 
+set /p userName=Enter user Name : 
+set /p loaclrepoName=Enter loacl repository Name : 
+set /p repoName=Enter repository Name : 
 
 git init
-git remote add %loaclName% https://github.com/tahaTWM/%repoName%
+
+git remote add %loaclrepoName% https://github.com/%userName%/%repoName%
+
+echo git remote add %loaclrepoName% https://github.com/%userName%/%repoName%
 
 git add -A
-set /p repoCommit=Enter your repo commant : 
-git commit -m "%repoCommit%"
-git push -u %localName% main
+
+set /p commit=Enter your commit : 
+echo %commit%
+
+git commit -m "%commit%"
+
+echo git push -u %loaclrepoName% main
+
+git branch -M main
+
+git push -u %loaclrepoName% main
+
 pause
